740e0a56c75462727af7299d0252b2a14eaa319f
OKeycode <100050457+OKeycode@users.noreply.github.com>
2022-02-20 11:23:49 +0100

    Update Kebaba.lua

--- Kebaba.lua
+++ Kebaba.lua
@@ -16,23 +16,97 @@ local Window = Library.CreateLib("Kebaber Ultimate v1", "Midnight")
     end)
 
     -- WINDOW
-    local Options = Window:NewTab("Options")
-    local Section = Options:NewSection("Toggles")
-  
-    Section:NewToggle("Auto Hatch", "Turns on", function(state)
-    if state then
-      _G.autohatch1 = true
-      while _G.autohatch1 == true do
-      local A_1 = 
-{
-	[1] = "Cracked Egg", 
-	[2] = false
-}
-local Event = game:GetService("Workspace")["__THINGS"]["__REMOTES"]["buy egg"]
-Event:InvokeServer(A_1)
-      wait()
-      end
-    else
-      _G.autohatch1 = false
+    local Options = Window:NewTab("AutoHatch")
+
+
+        local Section = Options:NewSection("Auto Hatch Switch")
+        Section:NewDropdown("On/Off", "Switch for Auto Hatching", {"On", "Off"}, function(v)
+            _G.autohatch = false;
+    while _G.autohatch == true do
+    local args = {
+        [1] = {
+            [1] = v,
+            [2] = true
+        }
+    }
+
+    workspace.__THINGS.__REMOTES:FindFirstChild("buy egg"):InvokeServer(unpack(args))
+    wait()
     end
-end)
+            
+        end)
+
+
+        local OptionsSection = Options:NewSection("Auto Hatch")
+        OptionsSection:NewDropdown("Axolotl Ocean", "Axolotl Eggs", {"Axolotl Egg", "Shiny Axolotl Egg"}, function(v)
+            _G.autohatch = true;
+    while _G.autohatch == true do
+    local args = {
+        [1] = {
+            [1] = v,
+            [2] = false
+        }
+    }
+
+    workspace.__THINGS.__REMOTES:FindFirstChild("buy egg"):InvokeServer(unpack(args))
+    wait()
+    end
+            
+        end)
+
+        OptionsSection:NewDropdown("Tech World", "Tech Eggs", {"Metal Egg", "Tech Egg", "Titanium Egg", "Dark Tech Egg", "Steampunk Egg", "Mechanical Egg", "Lab Egg", "Chemical Egg", "Alien Egg", "Martian Egg", "Planet Egg", "Glitch Egg", "Hacker Egg"}, function(v)
+            _G.autohatch = true;
+    while _G.autohatch == true do
+    local args = {
+        [1] = {
+            [1] = v,
+            [2] = false
+        }
+    }
+
+    workspace.__THINGS.__REMOTES:FindFirstChild("buy egg"):InvokeServer(unpack(args))
+    wait()
+    end
+            
+        end)
+
+        OptionsSection:NewDropdown("Fantasy World", "Fantasy Eggs", {"Enchanted Egg", "Relic Egg", "Samurai Egg", "Rainbow Egg", "Haunted Egg", "Hell Egg", "Heaven Egg", "Empyrean Egg"}, function(v)
+            _G.autohatch = true;
+    while _G.autohatch == true do
+    local args = {
+        [1] = {
+            [1] = v,
+            [2] = false
+        }
+    }
+
+    workspace.__THINGS.__REMOTES:FindFirstChild("buy egg"):InvokeServer(unpack(args))
+    wait()
+    end
+            
+        end)
+
+        OptionsSection:NewDropdown("Spawn World", "Spawn Eggs", {"Cracked Egg", "Spotted Egg", "Wood Egg", "Grass Egg", "Beachball Egg", "Coconut Egg", "Rock Egg", "Geode Egg", "Snow Egg", "Ice Egg", "Icicle Egg", "Yeti Egg", "Cactus Egg", "Spiked Egg", "Obsidian Egg", "Magma Egg", "Dominus Egg"}, function(v)
+            _G.autohatch = true;
+    while _G.autohatch == true do
+    local args = {
+        [1] = {
+            [1] = v,
+            [2] = false
+        }
+    }
+
+    workspace.__THINGS.__REMOTES:FindFirstChild("buy egg"):InvokeServer(unpack(args))
+    wait()
+    end
+            
+        end)
+
+        -- AUTOFARM
+    
+        local AutoFarm = Window:NewTab("AutoFarm")
+        local AutoFarmSection = AutoFarm:NewSection("AutoFarm")
+        AutoFarmSection:NewDropdown("Areas", "Autofarms The Selected Area", {"Town", "Forest", "Beach", "Mine", "Winter", "Glacier","Desert", "Volcano"}, function(currentOption)
+        
+        end)
+   
